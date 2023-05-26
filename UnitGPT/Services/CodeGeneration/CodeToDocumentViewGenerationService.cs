27623dc42976b04e0aa0f9f2f6924f07eeb00b4b
Henrik Andersson <henrik.a.andersson@tietoevry.com>
2023-05-26 14:32:57 +0200

    Updates

--- UnitGPT/Services/CodeGeneration/CodeToDocumentViewGenerationService.cs
+++ UnitGPT/Services/CodeGeneration/CodeToDocumentViewGenerationService.cs
@@ -1,4 +1,7 @@
-﻿using UnitGPT.Services.CodeGeneration.Interface;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitGPT.Services.CodeGeneration.Interface;
 using UnitGPT.Services.CodeGeneration.Models;
 
 namespace UnitGPT.Services.CodeGeneration;
@@ -15,9 +18,58 @@ internal class CodeToDocumentViewGenerationService : ICodeGenerationService
             return;
         }
 
-        docView.TextBuffer?.Insert(codeToDocumentGenerationModel.Position, Environment.NewLine + codeToDocumentGenerationModel.Code);
+        var currentUsings = GetAllUsings(docView.TextBuffer.CurrentSnapshot.GetText());
+        var (codeWithoutUsing, listOfUsing) = RemoveUsingFromGeneratedClass(codeToDocumentGenerationModel.Code);
+        docView.TextBuffer?.Insert(codeToDocumentGenerationModel.Position, Environment.NewLine + codeWithoutUsing);
+
+        foreach (var u in listOfUsing.Where(x => !currentUsings.Contains(x)))
+        {
+            docView.TextBuffer?.Insert(0, Environment.NewLine + u);
+        }
 
         await VS.Commands.ExecuteAsync("Edit.FormatDocument");
     }
+
+    public List<string> GetAllUsings(string input)
+    {
+        var result = new List<string>();
+        var lines = input.Split('\n');
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("class") || line.Contains("namespace")) break;
+            if (!line.Trim().StartsWith("using")) continue;
+            var parts = line.Trim().Split(' ');
+            var usingStatement = parts[0] + " " + parts[1];
+            result.Add(usingStatement);
+        }
+
+        return result;
+    }
+
+
+    private Tuple<string, List<string>> RemoveUsingFromGeneratedClass(string generatedCode)
+    {
+        var usingList = new List<string>();
+        var codeBuilder = new StringBuilder(generatedCode);
+        var usingIndex = 0;
+        var classIndex = generatedCode.IndexOf("class");
+        while ((usingIndex = codeBuilder.ToString().IndexOf("using")) >= 0)
+        {
+            var semicolonIndex = codeBuilder.ToString().IndexOf(";", usingIndex);
+            var usingStatement = codeBuilder.ToString().Substring(usingIndex, semicolonIndex - usingIndex + 1);
+
+            if (usingIndex >= classIndex) break;
+
+            usingList.Add(usingStatement);
+            codeBuilder.Remove(usingIndex, semicolonIndex - usingIndex + 1);
+
+        }
+
+        var codeWithoutUsings = codeBuilder.ToString().Trim();
+
+        return Tuple.Create(codeWithoutUsings, usingList);
+    }
+
 }
 
